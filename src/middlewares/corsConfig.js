45d200244ef73c2c8d8b7cd8a3e13d39230bdf8e
tmz81 <tmz.zt4@gmail.com>
2024-09-17 16:24:22 -0300

    change cors to *

--- src/middlewares/corsConfig.js
+++ src/middlewares/corsConfig.js
@@ -1,13 +1,5 @@
-const allowedOrigins = ["http://localhost:5173", "https://zap-ui-one.vercel.app/"];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "*",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
